b0f6f4e147a7d4029bf7a286aa6e22766dcdf0d4
Krokus <orlov-bn@mail.ru>
2024-09-07 23:41:32 +0500

    Домашнее задание по теме "DTL. Теги, наследование."

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -2,7 +2,7 @@
 URL configuration for UrbanDjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,17 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path
-from task2.views import option, Option2
-from task3.views import building_func, pipe_func, brick_func
-
+from task4.views import main_page, store, korzina
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', option),
-    path('option/', Option2.as_view()),
-    path('building/', building_func),
-    path('building/pipe/', pipe_func),
-    path('building/brick/', brick_func),
+    path("admin/", admin.site.urls),
+    path('platform/', main_page),
+    path('platform/store/', store),
+    path('platform/cart/', korzina)
 ]
