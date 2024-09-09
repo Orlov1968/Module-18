18556e52e4f59b182d407fcc684587be7f153bc5
Krokus <orlov-bn@mail.ru>
2024-09-09 23:15:40 +0500

    изменил форму переменной age

--- UrbanDjango/task5/forms.py
+++ UrbanDjango/task5/forms.py
@@ -5,4 +5,4 @@ class ContentForm(forms.Form):
     username = forms.CharField(max_length=30, label='Введите логин')
     password = forms.CharField(min_length=8, label='Введите пароль')
     repeat_password = forms.CharField(min_length=8, label='Повторите пароль')
-    age = forms.CharField(label='Введите свой возраст')
+    age = forms.IntegerField(label='Введите свой возраст')
